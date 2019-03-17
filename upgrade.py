3f2e075e8cfcc9b93568d3f6739b1eea1fb54d72
Daniel Ehlers <danielehlers@mindeye.net>
2019-03-17 19:16:35 +0100

    Fixup missing import

--- upgrade.py
+++ upgrade.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python3
 
-import requests
 import click
 from datetime import datetime, timedelta
+import logging
+import requests
+
 from ffua.graph import Graph,spantree,getLeafs
 from ffua.hopglass import getDataFromHopGlass
 
