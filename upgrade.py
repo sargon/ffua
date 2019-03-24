a5fb74cabffaf8bbb79aedef73d03974d5b0259a
Daniel Ehlers <danielehlers@mindeye.net>
2019-03-24 23:58:01 +0100

    Fiuxp pep8 complaints.

--- upgrade.py
+++ upgrade.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python3
 
-import click
 import logging
-import requests
 import sys
 
-from ffua.graph import Graph,spantree,getLeafs
+import click
+
+from ffua.graph import spantree
 from ffua.hopglass import getDataFromHopGlass
 from ffua.manifest import parse_manifest
 from ffua.mechanism import outerToInnerUpgrade, miauEnforce
@@ -13,10 +13,10 @@ from ffua.htaccess import generateHtAccessRules
 from ffua.config import Config
 
 @click.group()
-@click.option('--output','-o',type=click.File(mode='w'),default=sys.stdout)
-@click.option('--config','-c','config_file',type=click.File(mode='r'),prompt=True)
+@click.option('--output', '-o', type=click.File(mode='w'), default=sys.stdout)
+@click.option('--config', '-c', 'config_file', type=click.File(mode='r'), prompt=True)
 @click.pass_context
-def cli(ctx,output,config_file):
+def cli(ctx, output, config_file):
 
     config = Config()
     config.load(config_file)
@@ -25,16 +25,14 @@ def cli(ctx,output,config_file):
 
     graph = getDataFromHopGlass(hopglass)
     if len(startnode) > 1:
-        """
-        Construct virtual node and add a link to all startnodes.
-        """
+        # Construct virtual node and add a link to all startnodes.
         startident = graph.numNodes()
-        graph.setNodeIdent(startident,"_VIRTUAL")
+        graph.setNodeIdent(startident, "_VIRTUAL")
         num = 0
         for sn in startnode:
             try:
                 nodeid = graph.getGraphIdentFromIdent(sn)
-                graph.addEdge(startident,nodeid,[])
+                graph.addEdge(startident, nodeid, [])
                 num += 1
             except:
                 logging.warning(f"Node { sn } node found in network graph")
@@ -44,35 +42,35 @@ def cli(ctx,output,config_file):
     else:
         startident = graph.getGraphIdentFromIdent(startnode[0])
         ctx.obj['virtual_rootnode'] = False
-    tree = spantree(graph,startident)
+    tree = spantree(graph, startident)
     ctx.obj['graph'] = graph
     ctx.obj['tree'] = tree
     ctx.obj['output'] = output
 
 @cli.command(name="miauEnforce")
-@click.option("--min-distance","-d",type=click.INT,default=2)
-@click.argument("manifest_path",type=click.Path(exists=True,dir_okay=False))
+@click.option("--min-distance", "-d", type=click.INT, default=2)
+@click.argument("manifest_path", type=click.Path(exists=True, dir_okay=False))
 @click.pass_context
-def miau(ctx,min_distance,manifest_path):
+def miau(ctx, min_distance, manifest_path):
     manifest = parse_manifest(manifest_path)
     firmware_version = manifest.getFirmwareVersion().pop()
     firmware_branch = manifest.branch
-    print(f"#Tracking firmware version: { firmware_version }",file=ctx.obj['output'])
+    print(f"#Tracking firmware version: { firmware_version }", file=ctx.obj['output'])
     graph = ctx.obj['graph']
     tree = ctx.obj['tree']
     if ctx.obj['virtual_rootnode']:
         min_distance += 1
-    generator = miauEnforce(graph,tree,firmware_branch,firmware_version,min_distance)
-    generateHtAccessRules(generator,ctx.obj['output'])
-    
+    generator = miauEnforce(graph, tree, firmware_branch, firmware_version, min_distance)
+    generateHtAccessRules(generator, ctx.obj['output'])
+
 
 @cli.command(name="outerToInnerUpgrade")
 @click.pass_context
 def otiu(ctx):
     graph = ctx.obj['graph']
     tree = ctx.obj['tree']
-    generator = outerToInnerUpgrade(graph,tree)
-    generateHtAccessRules(generator,ctx.obj['output'])
+    generator = outerToInnerUpgrade(graph, tree)
+    generateHtAccessRules(generator, ctx.obj['output'])
 
 if __name__ == "__main__":
     cli(obj={})
