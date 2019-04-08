7460e82a4e73f16831c0b9616b84491dd41d5683
Daniel Ehlers <danielehlers@mindeye.net>
2019-04-08 22:44:54 +0200

    Make firmware_path configurable and parse manifests of configured branches

--- ffua/config.py
+++ ffua/config.py
@@ -1,10 +1,15 @@
 import attr
 import json
+from pathlib import Path
+
+from ffua.manifest import parse_manifest
 
 @attr.s
 class Config:
     hopglass = attr.ib(default="http://localhost:4000/")
     startnodes = attr.ib(factory=list)
+    firmware_path = attr.ib(default=Path("/opt/firmware/"))
+    branches = attr.ib(factory=list)
 
     def load(self,config_file):
         config = json.load(config_file)
@@ -16,3 +21,14 @@ class Config:
             self.startnodes = config['startnodes']
         else:
             raise Exception("Missing startnodes in config")
+        if "firmware_path" in config:
+            self.firmware_path = Path(config['firmware_path'])
+        if "branches" not in config:
+            self.branches['stable'] = parse_manifest(self.firmware_path / "stable" / "sysupgrade" / "stable.manifest")
+        else:
+            for branch in config['branches']:
+                self.branches[branch] = parse_manifest(self.firmware_path / branch / "sysupgrade" / f"{ branch }.manifest")
+
+
+    def get_branch(self,branch):
+        return self.branches[branch]
