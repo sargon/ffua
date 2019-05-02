492d65f918d6dea19ef34a3c3dca0e1bf5d02a12
Daniel Ehlers <danielehlers@mindeye.net>
2019-05-03 00:11:42 +0200

    Make outerToInnerUpgrade call compatibility aware.

--- upgrade.py
+++ upgrade.py
@@ -4,11 +4,10 @@ import logging
 import sys
 
 import click
-
-from ffua.graph import spantree, addVirtualNode
+from ffua.graph import spantree, addVirtualNode, getSubtreeNodes, getTreeSubtrees, clone_graph
 from ffua.hopglass import getDataFromHopGlass
 from ffua.mechanism import outerToInnerUpgrade, miauEnforce
-from ffua.htaccess import generateHtAccessRulesForBranches
+from ffua.htaccess import generateHtAccessRulesForBranches, generateHtAccessRulesForBranch
 from ffua.config import Config
 
 @click.group()
@@ -38,6 +37,7 @@ def cli(ctx, debug, config_file):
     ctx.obj['config'] = config
     ctx.obj['graph'] = graph
     ctx.obj['tree'] = tree
+    ctx.obj['rootnode'] = startnode
 
 @cli.command(name="miauEnforce")
 @click.option("--min-distance", "-d", type=click.INT, default=2)
@@ -55,9 +55,23 @@ def miau(ctx, min_distance):
 @click.pass_context
 def otiu(ctx):
     graph = ctx.obj['graph']
-    tree = ctx.obj['tree']
-    generator = outerToInnerUpgrade(graph, tree,ctx.obj['config'].branches)
-    generateHtAccessRulesForBranches(generator, ctx.obj['config'])
+    config = ctx.obj['config']
+    
+    subtrees = list(getTreeSubtrees(graph,ctx.obj['tree'],ctx.obj['rootnode']))
+    for branch in config.branches:
+        tree = clone_graph(ctx.obj['tree'])
+        num_compatible = 0
+        for st in subtrees:
+            for stn in getSubtreeNodes(tree,st):
+                data = graph.getNodeData(stn)
+                if data.getBranch() in config.incompatible[branch]:
+                    for stn in getSubtreeNodes(tree,st):
+                        std = graph.getNodeData(stn)
+                        tree.removeNode(stn)
+                    break
+        logging.debug(f"Branch: { branch } ; Nodes in Tree: { tree.numNodes() }")
+        generator = outerToInnerUpgrade(graph, tree,ctx.obj['config'].branches)
+        generateHtAccessRulesForBranch(branch,generator, ctx.obj['config'])
 
 if __name__ == "__main__":
     cli(obj={})
