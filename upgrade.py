73bd1cc442e3b8f9765cf7283b922413d72e77ed
Daniel Ehlers <danielehlers@mindeye.net>
2019-05-12 19:07:18 +0200

    Adapt mechansim refactoring

--- upgrade.py
+++ upgrade.py
@@ -6,15 +6,16 @@ import sys
 import click
 from ffua.graph import spantree, addVirtualNode, getSubtreeNodes, getTreeSubtrees, clone_graph
 from ffua.hopglass import getDataFromHopGlass
-from ffua.mechanism import outerToInnerUpgrade, miauEnforce
+from ffua.mechanism import mechanismFactory, mechansim_dict
 from ffua.htaccess import generateHtAccessRulesForBranches, generateHtAccessRulesForBranch
 from ffua.config import Config
+from ffua.upgrade import UpgradeModel
 
-@click.group()
+@click.command()
 @click.option("--debug/--no-debug",default=False,help="Debugging output")
 @click.option('--config', '-c', 'config_file', type=click.File(mode='r'), prompt=True)
-@click.pass_context
-def cli(ctx, debug, config_file):
+@click.argument('mechanism', default="outerToInnerUpgrade",type=click.Choice(mechansim_dict.keys()))
+def cli(debug, config_file,mechanism):
     if debug:
         logging.basicConfig(level=logging.DEBUG)
     else:
@@ -26,52 +27,18 @@ def cli(ctx, debug, config_file):
     startnode = config.startnodes
 
     graph = getDataFromHopGlass(hopglass)
-    if len(startnode) > 1:
+    if config.has_virtal_rootnode():
         # Construct virtual node and add a link to all startnodes.
         startnode = addVirtualNode(graph,startnode)
-        ctx.obj['virtual_rootnode'] = True
     else:
         startnode = graph.getGraphIdentFromIdent(startnode[0])
-        ctx.obj['virtual_rootnode'] = False
     tree = spantree(graph, startnode)
-    ctx.obj['config'] = config
-    ctx.obj['graph'] = graph
-    ctx.obj['tree'] = tree
-    ctx.obj['rootnode'] = startnode
 
-@cli.command(name="miauEnforce")
-@click.option("--min-distance", "-d", type=click.INT, default=2)
-@click.pass_context
-def miau(ctx, min_distance):
-    firmware = [ (name,mfst.getFirmwareVersion().pop()) for name,mfst in ctx.obj['config'].branches.items()]
-    graph = ctx.obj['graph']
-    tree = ctx.obj['tree']
-    if ctx.obj['virtual_rootnode']:
-        min_distance += 1
-    generator = miauEnforce(graph, tree, firmware, min_distance)
-    generateHtAccessRulesForBranches(generator, ctx.obj['config'])
+    mechanism = mechanismFactory(mechanism,config)
+    upgrade = UpgradeModel(graph,tree,mechanism)
+    for branch, generator in upgrade(config):
+        generateHtAccessRulesForBranch(branch, generator, config)
 
-@cli.command(name="outerToInnerUpgrade")
-@click.pass_context
-def otiu(ctx):
-    graph = ctx.obj['graph']
-    config = ctx.obj['config']
-    
-    subtrees = list(getTreeSubtrees(graph,ctx.obj['tree'],ctx.obj['rootnode']))
-    for branch in config.branches:
-        tree = clone_graph(ctx.obj['tree'])
-        num_compatible = 0
-        for st in subtrees:
-            for stn in getSubtreeNodes(tree,st):
-                data = graph.getNodeData(stn)
-                if data.getBranch() in config.incompatible[branch]:
-                    for stn in getSubtreeNodes(tree,st):
-                        std = graph.getNodeData(stn)
-                        tree.removeNode(stn)
-                    break
-        logging.debug(f"Branch: { branch } ; Nodes in Tree: { tree.numNodes() }")
-        generator = outerToInnerUpgrade(graph, tree,ctx.obj['config'].branches)
-        generateHtAccessRulesForBranch(branch,generator, ctx.obj['config'])
 
 if __name__ == "__main__":
     cli(obj={})
